a41456da8f698a795f4809d56839a3354b62ef8f
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2021-01-16 10:03:54 +0530

    common styles extracted to common file

--- client/src/components/user/AboutText/index.jsx
+++ client/src/components/user/AboutText/index.jsx
@@ -7,25 +7,24 @@ const AboutText = () => {
         <section className="about-text p-2">
             <div className="container set-padding">
                 <p><span className="tab"></span>
-                    We at Syncadvert, revel at the fact that the company's quintessential work has been our delighted
-                    achievement. The suave and scintillating pictures of the photographer and his ilk never cease to
-                    amaze the ardent eyes. We work with the epitome of elan to provide the best of the artistic world to
-                    you. Uncompromising and unsparing, we thrive under equanimity and ensure only the choicest upshot
-                    are delivered.
+                    At Syncadvert, we revel at the fact that the company's quintessential work has been our delighted achievement. 
+                    The suave and scintillating pictures captured by our photographers never cease to amaze the ardent eyes. 
+                    We work with the epitome of elan to provide the best of the artistic world to you. 
+                    Uncompromising and unsparing, we thrive under equanimity to ensure the beautiful moments are captured using the best of modern technology, 
+                    and only the choicest upshots are delivered.
                 </p>
-                <p className="mt-5"><span className="tab"></span>
-                    Take a look at the world of idyllic perfection that's carefully captured by the creative eyes of Dr
-                    Siddarth Thota from Phi creations who is also our creative head, whose passion towards photography
-                    has helped him catch the most inestimable moments of joy! The work of art at display is essentially
-                    captured through the best of modern technology. 
+                <p><span className="tab"></span>
+                    <em>Dr. Siddarth Thota</em> <span className="job-title">(creative head)</span> and his passion for photography have always been the backbone of creativity at Syncadvert. 
+                    He diligently plans the best locations and the most inestimable moments of joy to be captured with idyllic perfection. 
                 </p>
-                <p className="mt-5"><span className="tab"></span>
-                    At Syncadvert, the team headed by Mr Pradhyumna, our
-                    director, aims at providing our customers the best of services ranging from weddings, engagements,
-                    photoshoots and much more! From capturing the daunting beauty of a shy bride that resonates with the
-                    charismatic charm of a new groom to an action packed hustling of a mind boggling cinematic
-                    experience, we at Syncadvert are here to cater to you an experience that will teleport you to the
-                    world of perfection while unraveling many memories that you will echo and reminisce.
+                <p><span className="tab"></span>
+                    At Syncadvert, <em>Mr. Pradhyumna</em> <span className="job-title">(Director)</span> and his team aim at providing our customers the best of services ranging from weddings, 
+                    engagements, photoshoots and much more! From capturing the enchanting beauty of a shy bride that resonates with 
+                    the charismatic charm of a new groom to an action packed hustling of a mind boggling cinematic experience.
+                </p>
+                <p><span className="tab"></span>
+                    We at Syncadvert are here to cater to an experience that transcends your expectations while unraveling many memories that you will 
+                    cherish and reminisce.
                 </p>
             </div>
         </section>
